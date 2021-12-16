c79b31cdf8f4d59ca828de619cc1edb820249604
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2021-12-16 09:59:01 +0100

    initial code

--- R/read-funcs.R
+++ R/read-funcs.R
@@ -1,4 +1,20 @@
-read_rpkmx <- function(path){
+read_pkmx <- function(path, type = NULL, ...){
+  if(is.null(type))
+    type <- tools::file_ext(path)
+  type <- match.arg(type, c("rpkmx", "lpkmx", "dpkmx"))
+  read_func <- switch(type,
+    "rpkmx" = read_rpkmx,
+    "lpkmx" = read_lpkmx,
+    "dpkmx" = read_dpkmx
+  )
+  read_func(path, ...)
+}
+
+read_lpkmx <- function(path, ...){
+  readLines(path, ...)
+}
+
+read_rpkmx <- function(path, clean_names = TRUE, ...){
   dtl <- readLines(path)
   dtl <- gsub(";", ",", dtl)
   dtl <- gsub("=", ":", dtl)
@@ -27,7 +43,46 @@ read_rpkmx <- function(path){
       dtl[i] <- sprintf('"%s":{', strsplit(dtl[i], ":\\{")[[1]])
     }
   }
-  browser()
+  dtl <- c("[", dtl, "]")
+  dt <- clean_rpkmx(dtl, clean_names = clean_names)
+  id <- gsub(paste0(".", tools::file_ext(path)), "", basename(path))
+  dplyr::tibble(file = id,
+         dt)
+}
 
-  dt <- jsonlite::parse_json(c("[", dtl, "]"))
+read_dpkmx <- function(path, ...){
+  stop("We have not implemented a data reader for this file type yet.")
+}
+
+
+## Utils ----
+
+clean_rpkmx <- function(dtl, clean_names = TRUE){
+  dt <- jsonlite::fromJSON(dtl)
+  dt <- dplyr::as_tibble(dt)
+  if(clean_names)
+    dt <- janitor::clean_names(dt)
+  dt_flat <- flatten_rpkmx(dt, clean_names = clean_names)
+  type.convert(dt_flat)
+}
+
+flatten_rpkmx <- function(data, clean_names = TRUE){
+  idx <- which(unlist(lapply(data, is.data.frame)))
+  dt <- data
+  for(i in rev(idx)){
+    tmp <- data[,i][[1]]
+    if(class(tmp) == "character"){
+      tmp <- data[,i]
+    }
+    nm <- names(data)[i]
+    if(any(sapply(tmp, is.data.frame))){
+      tmp <- flatten_rpkmx(tmp)
+    }
+    if(clean_names)
+      tmp <- janitor::clean_names(tmp)
+    names(tmp) <- sprintf("%s_%s", nm, names(tmp))
+    dt <- cbind(dt, tmp)
+    dt[, i] <- NULL
+  }
+  dplyr::as_tibble(dt)
 }
